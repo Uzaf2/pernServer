a1cfcd07fe0321bf125b9bc232fd07def20a8397
Usman Zafar <Uzman.Zafar1@gmail.com>
2021-01-03 21:33:32 +1100

    Another commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports={
+    password: "fc41594e0e63b7e94e92ced61b7997f44c0510e573dcf91e7ae6cd1112d94507",
+    user: "rkpzqzrgflicxz",
+    host: "ec2-52-0-65-165.compute-1.amazonaws.com",
+    database:"dft26cp2nvmf28",
+    port: 5432,
+    ssl: true
+}
